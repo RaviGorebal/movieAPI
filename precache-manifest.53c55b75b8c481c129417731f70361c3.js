3878013b7aa9d9758cd178370fb0a64cfb27d260
Ravi Kumar G <ravikumarg@maveric-systems.com>
2020-07-07 11:44:55 +0530

    Updates

---
+++ precache-manifest.53c55b75b8c481c129417731f70361c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1bf3fb477fedf56069e777e62339f3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f82ca83890af8a36a34",
+    "url": "/static/css/main.35cda0c4.chunk.css"
+  },
+  {
+    "revision": "dc1ca908f8b04b5218fd",
+    "url": "/static/js/2.423f09b9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.423f09b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f82ca83890af8a36a34",
+    "url": "/static/js/main.96489d8b.chunk.js"
+  },
+  {
+    "revision": "43879a7175c3e740616a",
+    "url": "/static/js/runtime-main.686b7e5d.js"
+  }
+]);
